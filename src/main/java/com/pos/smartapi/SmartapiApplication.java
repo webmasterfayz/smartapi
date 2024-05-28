9c93de7345ab74451780bff8db4a897b389d48a9
Monir Ahmed Fayz <webmasterfayz@gmail.com>
2024-05-28 15:48:29 +0600

    first commit

---
+++ src/main/java/com/pos/smartapi/SmartapiApplication.java
@@ -0,0 +1,13 @@
+package com.pos.smartapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartapiApplication.class, args);
+	}
+
+}
